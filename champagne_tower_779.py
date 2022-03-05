6cd5a7f7ee722ab63edac348e6c0c66d507238ff
Yashin Ilya <imyashin1996@gmail.com>
2022-03-05 22:07:07 +0300

    779 right answer but time limit error

--- champagne_tower_779.py
+++ champagne_tower_779.py
@@ -1,49 +1,57 @@
-from typing import List
-
-
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        # def champagneTower(self):
+        tower = dict()
+
         class Node:
-            def __init__(self, row, glass):
-                self.position = row, glass
-                self.left = row + 1, glass
-                self.right = row + 1, glass + 1
-                self.value = 0
-                self.parents_count = 1 if any([row == glass, glass == 0]) else 2
-                # TODO Добавить метод add(), который увеличивает value, а если оно превышает 1, вызывает add() для детей
-                # TODO изменить структуру на древовидную
-                # TODO избавиться от лишних полей, добавить @property filled: bool
+            def __init__(self, row, glass, *, value=0):
+                self.row = row
+                self.glass = glass
+                self.value = value
+                tower[(row, glass)] = self
+                try:
+                    self.left = tower[(row + 1, glass)]
+                except KeyError:
+                    self.left = None
+                try:
+                    self.right = tower[(row + 1, glass + 1)]
+                except KeyError:
+                    self.right = None
+                try:
+                    tower[(row - 1, glass - 1)].right = self
+                except KeyError:
+                    pass
+                try:
+                    tower[(row - 1, glass)].left = self
+                except KeyError:
+                    pass
 
-            def __str__(self):
-                return f'{self.position}, v: {self.value}'
+            @property
+            def is_full(self):
+                return 1 - self.value < 10 ** -10
 
-        # tower = [] #Node
-        tower: List[List[Node]] = []
-        for num in range(100):
-            row = [Node(num, glass) for glass in range(num + 1)]
-            tower.append(row)
+            def create_children(self, *, adding=0):
+                if self.left is None:
+                    self.left = Node(self.row + 1, self.glass, value=adding)
+                if self.right is None:
+                    self.right = Node(self.row + 1, self.glass + 1, value=adding)
 
-        cur_filling_glasses: List[Node] = [tower[0][0]]
-        stream_count = 1
+            def adding(self, adding_volume):
+                if self.is_full:
+                    self.left.adding(adding_volume / 2)
+                    self.right.adding(adding_volume / 2)
+                elif self.value + adding_volume >= 1:
+                    extra = self.value + adding_volume - 1
+                    self.value = 1
+                    self.create_children()
+                    self.left.adding(extra / 2)
+                    self.right.adding(extra / 2)
+                else:
+                    self.value += adding_volume
 
+        root = Node(0, 0)
         for i in range(poured):
-            future_filling_glasses = []
-            future_stream_count = stream_count
-            adding_part = 1 / stream_count
-            for glass in cur_filling_glasses:
-                glass.value += adding_part
-                if round(glass.value, 5) >= 1:
-                    while glass in cur_filling_glasses:
-                        cur_filling_glasses.remove(glass)
-                    future_stream_count += 2 - glass.parents_count
-                    future_filling_glasses.append(tower[glass.left[0]][glass.left[1]])
-                    future_filling_glasses.append(tower[glass.right[0]][glass.right[1]])
-
-            cur_filling_glasses.extend(future_filling_glasses)
-            stream_count = future_stream_count
-
-        return tower[query_row][query_glass].value
-
-
-print(Solution().champagneTower(*map(int, input().split())))
+            root.adding(1)
+        temp = tower.get((query_row, query_glass))
+        if temp is None:
+            return 0
+        return temp.value
