8efd075f918be15cde13b56d83240e3fe767a01e
Yashin Ilya <imyashin1996@gmail.com>
2022-03-16 01:06:43 +0300

    1249 fixed calculate time

--- minimum_remove_to_make_valid_parentheses_1249.py
+++ minimum_remove_to_make_valid_parentheses_1249.py
@@ -1,25 +1,27 @@
-def remove_parentheses(s: str) -> str:
-    answer = ''
-    stack = []
-    alone_pars = 0
-    for num, char in enumerate(s):
-        if char != ')' and char != '(':
-            answer += char
-        elif char == '(':
-            if s[num + 1:].count(')') > alone_pars:
-                alone_pars += 1
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        answer = ''
+        stack = []  # Стэк для проверки того, что перед закрывающей скобкой есть парная открывающая
+        alone_pars = 0
+        count_sad = 0
+        all_count_sad = s.count(')')
+        for num, char in enumerate(s):
+            if char != ')' and char != '(':
                 answer += char
-                stack.append('(')
-
-        else:
-            if stack and stack[-1] == '(':
-                answer += char
-                del stack[-1]
-                if alone_pars:
-                    alone_pars -= 1
-
-    return answer
-
-
-for _ in range(10):
-    print(remove_parentheses(input()))
+            elif char == '(':
+                # Открывающая скобка добавляется только в том случае, 
+                # если в строке еще имеется соответствующая закрывающая скобка
+                if all_count_sad - count_sad > alone_pars: 
+                    alone_pars += 1
+                    answer += char
+                    stack.append('(')
+            else:
+                # Проверка на то, что перед закрывающей скобкой была свободная открывающая
+                # тем самым отсекаются выражения вида *(*)*)*
+                if stack and stack[-1] == '(':
+                    answer += char
+                    del stack[-1]
+                    if alone_pars:
+                        alone_pars -= 1
+                count_sad += 1
+        return answer
