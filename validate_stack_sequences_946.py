f8696c844ae120d206090dcb70c0dee0fd850979
Yashin Ilya <imyashin1996@gmail.com>
2022-03-17 00:46:44 +0300

    946 fixed calculating time

--- validate_stack_sequences_946.py
+++ validate_stack_sequences_946.py
@@ -2,16 +2,16 @@ from typing import List
 
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        stack = [pushed.pop(0)]
+        pushed = list(reversed(pushed))
+        stack = [pushed.pop()]
         for pop_el in popped:
             if stack and stack[-1] == pop_el:
                 del stack[-1]
             elif pushed:
-                while pop_el != (new_stack_el := pushed.pop(0)):
+                while pop_el != (new_stack_el := pushed.pop()):
                     stack.append(new_stack_el)
                     if not pushed:
                         break
             else:
                 return False
         return True
-
